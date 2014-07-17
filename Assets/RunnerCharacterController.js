e23b3ced906e6046124ad6b4e7932b427491e274
Christopher Silich <chris.silich@gmail.com>
2014-07-16 22:44:34 -0400

    done

--- Assets/RunnerCharacterController.js
+++ Assets/RunnerCharacterController.js
@@ -7,19 +7,14 @@ var jumpForce = 10;
 
 private var currentSpeed:float = startVelocity;
 
-var groundRayOffset:float = 1.48;
-var groundRayHeight:float = 0.02;
+var groundRayOffset:float = 1.1;
+
 
 function Start () {
 	rigidbody2D.velocity.x = startVelocity;
 }
 
-// Frame update example: Draws a 10 meter long green line from the position for 1 frame.
-function Update () {
-	var start = transform.position;
-	start.y -= groundRayOffset;
-	Debug.DrawRay(start, -Vector2.up * groundRayHeight, Color.green, 0.1);
-}
+
 
 function FixedUpdate () {
 	if (currentSpeed < maxVelocity) {
@@ -27,20 +22,29 @@ function FixedUpdate () {
 	}
 	rigidbody2D.velocity.x = currentSpeed;
 	
+	var start = transform.position;
+	start.y -= groundRayOffset;
+		
+	Debug.DrawRay(start, -Vector2.up, Color.red, 1);
+	var hit:RaycastHit2D = Physics2D.Raycast(start, -Vector2.up, 0.03);
+	
+	var grounded = false;
+	if (hit.collider != null && hit.collider.tag == "Ground" /* optional */) {
+		grounded = true;
+	}
+	
+	var anim:Animator = this.GetComponent("Animator");
+	if (grounded) {
+		anim.SetInteger("state", 0);
+	} else {
+		anim.SetInteger("state", 1);
+	}
+	
 	if (Input.GetAxis("Vertical")) {
 		Debug.Log("jump");
 		
-		var start = transform.position;
-		start.y -= groundRayOffset;
-	
-		var hit:RaycastHit2D = Physics2D.Raycast(start, -Vector2.up, groundRayHeight);
-		
-		Debug.DrawRay(start, -Vector2.up * groundRayHeight, Color.red, 1);
-		
-		if (hit.collider != null && hit.collider.tag == "Ground") {
-			Debug.Log("hit");
-			Debug.Log(hit.collider);
-			rigidbody2D.AddForce(Vector2.up * jumpForce);
+		if (grounded) {
+			//rigidbody2D.AddForce(Vector2.up * jumpForce);
 			rigidbody2D.velocity.y = jumpForce;
 		}
 	}
